826a78358685a63b37e641cecfee29257dc410c4
FXXYYY <46418828+FXXYYY@users.noreply.github.com>
2019-01-09 09:06:37 +0800

    Add files via upload

---
+++ #整体框架.py
@@ -0,0 +1,43 @@
+#代码整体框架
+
+#引用库
+
+#创建窗口
+
+
+def GameStart():
+
+
+    #游戏背景对象
+    
+    Background = pygame.image.load()
+    
+    #挡板背景对象
+
+    Baddle = pygame.image.load()
+
+    #球对象    
+
+    Ball = pygame.image.load()
+
+    #挡板位置信息
+
+    BaffleX
+    BaffleY
+
+    #球位置信息
+
+    BallX
+    ballY
+    BallSpeed
+
+    #计算时间
+
+def GameResult():
+
+
+    #
+
+
+
+if __name__ == "__main__":
